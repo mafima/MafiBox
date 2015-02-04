83f3acfa9c5512be9bbcce21b0ded091550b7f9c
Cobra_8 <bl.bl@rega-sense.ch>
2015-02-04 18:10:16 +0100

    Version by Cobra_8
    Updated for NetBeans

--- src/MafiBox/ClickListener.java
+++ src/MafiBox/ClickListener.java
@@ -3,12 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.mafi.MafiBox;
+package mafiBox;
 
 import java.util.ArrayList;
-
 import net.milkbowl.vault.economy.Economy;
-
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,62 +23,51 @@ import org.bukkit.plugin.RegisteredServiceProvider;
  */
 public class ClickListener implements Listener {
 
-    private final ArrayList<String> playersToAdd = new ArrayList<>();
-    private final RegisteredServiceProvider<Economy> eProvider = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
-    private final Economy e = eProvider.getProvider();
+    private final ArrayList<String> playersToAdd;
+    private final RegisteredServiceProvider<Economy> provider;
+    private final Economy economy;
+
+    public ClickListener() {
+        playersToAdd = new ArrayList<>();
+        provider = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
+        economy = provider.getProvider();
+    }
 
     @EventHandler
-    public void onInteract(PlayerInteractEvent evt) {
+    public void onInteract(PlayerInteractEvent e) {
+        if (!(e.hasItem())) {
+            return;
+        }
+        ItemStack item = e.getItem();
+        if (!(item.getItemMeta().getDisplayName().equalsIgnoreCase(MafiBox.boxprefix))) {
+            return;
+        }
+        e.setCancelled(true);
 
-        if (evt.hasItem()) {
-            ItemStack item = evt.getItem();
-            if (item.getItemMeta().getDisplayName().equalsIgnoreCase(Main.boxprefix)) {
-
-                evt.setCancelled(true);
-
-                if (e.has(evt.getPlayer(), Main.price)) {
-                	
-                	
-                	// beispiel:
-        			double diamant = 0.4;
-        			double gold = 0.5;
-        			double stein = (1 - gold + diamant);
-        			double E1 = (double) (Math.random() * stein);
-        			double E2 = (double) (Math.random() * gold);
-        			double E3 = (double) (Math.random() * diamant);
-
-        			if (E1 > E2 && E2 > E3) {
-        				System.out.println("Du bekommst ein Steinschwert");
-        			} if (E2 > E1 && E2 > E3) {
-        				System.out.println("Du bekommst ein Goldschwert");
-        			} if (E3 > E1 && E3 > E2) {
-        				System.out.println("Du bekommst ein Diamantschwert");
-        			}
-
-                } else {
-                    evt.getPlayer().sendMessage(Main.prefix + "§4Du hast leider nicht genug Mafis :(");
-                }
+        if (economy.has(e.getPlayer(), MafiBox.price)) {
+            int x = (int) Math.ceil(Math.random() * 100);
+            if (x > 99) {//99 % chancen
 
             }
 
+        } else {
+            e.getPlayer().sendMessage(MafiBox.prefix + "§4Du hast leider nicht genug Mafis :(");
         }
 
     }
 
     @EventHandler
     public void onDeath(PlayerDeathEvent evt) {
-        if (evt.getDrops().contains(Main.item)) {
-            evt.getDrops().remove(Main.item);
-
+        if (evt.getDrops().contains(MafiBox.item)) {
+            evt.getDrops().remove(MafiBox.item);
             playersToAdd.add(evt.getEntity().getName());
-
         }
 
     }
 
     @EventHandler
     public void onDrop(PlayerDropItemEvent evt) {
-        if (evt.getItemDrop().getItemStack() == Main.item) {
+        if (evt.getItemDrop().getItemStack().isSimilar(MafiBox.item)) {
             evt.setCancelled(true);
             evt.getPlayer().updateInventory();
         }
@@ -90,7 +77,7 @@ public class ClickListener implements Listener {
     public void onRespawn(PlayerRespawnEvent evt) {
         if (playersToAdd.contains(evt.getPlayer().getName())) {
 
-            evt.getPlayer().getInventory().addItem(Main.item);
+            evt.getPlayer().getInventory().addItem(MafiBox.item);
             evt.getPlayer().updateInventory();
 
             playersToAdd.remove(evt.getPlayer().getName());
--- src/mafiBox/MafiBox.java
+++ src/mafiBox/MafiBox.java
@@ -3,10 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.mafi.MafiBox;
+package mafiBox;
 
 import java.util.Arrays;
-import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -15,20 +14,17 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *
  * @author Manuel
  */
-public class Main extends JavaPlugin {
+public class MafiBox extends JavaPlugin {
 
     public static final int price = 10000;
-    public static String prefix = "§8[§6Mafi§cBox§8]§r ";
+    public static final String prefix = "§8[§6Mafi§cBox§8]§r ";
     public static final String boxprefix = "§a§l- §oMafiBox §a§l-";
-
-    private ClickListener clicklistener;
 
     public static ItemStack item;
 
@@ -36,12 +32,7 @@ public class Main extends JavaPlugin {
     public void onEnable() {
 
         setupItemStack();
-
-        clicklistener = new ClickListener();
-
-        this.getServer().getPluginManager().registerEvents(clicklistener, this);
-
-        super.onEnable(); //To change body of generated methods, choose Tools | Templates.
+        this.getServer().getPluginManager().registerEvents(new ClickListener(), this);
     }
 
     @Override
@@ -51,15 +42,15 @@ public class Main extends JavaPlugin {
                 Player p = (Player) sender;
 
                 if (p.getInventory().contains(item)) {
-
-                    p.sendMessage(prefix + "§8Rechtsklicke die Box in deinem Iventar!");
-                    return true;
+                    p.getInventory().remove(item);
+                    p.sendMessage(prefix + "§6Du hast deine MafiBox weg gepackt.");
                 }
 
                 p.getInventory().addItem(item);
                 p.updateInventory();
                 p.playEffect(p.getLocation(), Effect.SMOKE, p);
                 p.playSound(p.getLocation(), Sound.NOTE_PLING, 5, 3);
+                p.sendMessage(prefix + "§6Du findest nun eine MafiBox in deinem Inventar.");
             }
         }
         return true;
